5c4eb81310ee209794dc5dc4f941d676120f0852
mjkim <mjkim9334@gmail.com>
2022-07-07 10:02:45 +0900

    db

--- db1.py
+++ db1.py
@@ -10,6 +10,13 @@ cur = con.cursor()
 cur.execute("create table PhoneBook (Name text, PhoneNum text);")
 # 1건 입력
 cur.execute("insert into PhoneBook values ('derick', '010-222');")
+#입력 파라메터 처리
+name = "gildong"
+phoneNumber = "010-123"
+cur.execute("insert into PhoneBook values (?, ?);", (name, phoneNumber))
+# 다중 레코드 입력
+datalist = (("tom", "010-345"), ("dsp", "010-333"))
+cur.executemany("insert into PhoneBook values (?, ?);", datalist)
 
 # 검색
 cur.execute("select * from PhoneBook;")
